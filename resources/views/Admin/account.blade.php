d87d18ae350742267bb1f9b36ec7b9bc0bef5096
Gurleen Kaur <110157376+Guruleen02kaur@users.noreply.github.com>
2023-04-18 23:48:17 +0530

    PAssword

--- resources/views/Admin/account.blade.php
+++ resources/views/Admin/account.blade.php
@@ -15,77 +15,96 @@
 <li class="breadcrumb-item">Users</li>
 <li class="breadcrumb-item active">Edit Profile</li>
 @endsection
-
 @section('content')
 <div class="container-fluid">
-            <div class="edit-profile">
-              <div class="row">
-                <div class="col-xl-4">
-                  <div class="card">
-                    <div class="card-header">
-                      <h4 class="card-title mb-0">My Profile</h4>
-                      <div class="card-options"><a class="card-options-collapse" href="#" data-bs-toggle="card-collapse"><i class="fe fe-chevron-up"></i></a><a class="card-options-remove" href="#" data-bs-toggle="card-remove"><i class="fe fe-x"></i></a></div>
+    <div class="row">
+        <div class="card" >
+        <div class="card-header">
+            <h4 class="card-title mb-0">Edit Profile</h4>
+            <div class="card-options"><a class="card-options-collapse" href="#" data-bs-toggle="card-collapse"><i class="fe fe-chevron-up"></i></a><a class="card-options-remove" href="#" data-bs-toggle="card-remove"><i class="fe fe-x"></i></a></div>
+        </div>
+        <div class="card-body">
+            <form action="" onsubmit="return checkForm();">@csrf
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="mb-3">
+                    <label class="form-label">Name</label>
+                    <input class="form-control" type="text" value="{{$data->name}}" placeholder="Name">
                     </div>
-                    <div class="card-body">
-                        <div class="row mb-2">
-                          <div class="profile-title">
-                            <div class="media">                        <img class="img-70 rounded-circle" alt="" src="{{ asset('assets/images/user/7.jpg') }}">
-                              <div class="media-body">
-                                <h5 class="mb-1">{{$data->name}}</h5>
-                                <p>ADMIN</p>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-xl-8">
-                  <form class="card">
-                    <div class="card-header">
-                      <h4 class="card-title mb-0">Edit Profile</h4>
-                      <div class="card-options"><a class="card-options-collapse" href="#" data-bs-toggle="card-collapse"><i class="fe fe-chevron-up"></i></a><a class="card-options-remove" href="#" data-bs-toggle="card-remove"><i class="fe fe-x"></i></a></div>
-                    </div>
-                    <div class="card-body">
-                      <div class="row">
-                        <div class="col-md-5">
-                          <div class="mb-3">
-                            <label class="form-label">Name</label>
-                            <input class="form-control" type="text" value="{{$data->name}}" placeholder="Name">
-                          </div>
-                        </div>
-
-                        <div class="col-sm-6 col-md-4">
-                          <div class="mb-3">
-                            <label class="form-label">Email address</label>
-                            <input class="form-control" type="email" value="{{$data->email}}" placeholder="Email">
-                          </div>
-                        </div>
-                        <div class="col-sm-6 col-md-4">
-                          <div class="mb-3">
-                            <label class="form-label">Password</label>
-                            <input class="form-control" type="password"  placeholder="Password">
-                          </div>
-                        </div>
-                        <div class="col-md-12">
-                          <div>
-                            <label class="form-label">About Me</label>
-                            <textarea class="form-control" rows="4" placeholder="Enter About your description"></textarea>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="card-footer text-end">
-                      <button class="btn btn-primary" type="submit">Update Profile</button>
-                    </div>
-                  </form>
                 </div>
 
-              </div>
+                <div class="col-sm-6">
+                    <div class="mb-3">
+                    <label class="form-label">Email address</label>
+                    <input class="form-control" type="email" value="{{$data->email}}" placeholder="Email">
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="mb-3">
+                    <label class="form-label">Old Password</label>
+                    <input class="form-control" type="text" id="oldP"placeholder="Old Password">
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="mb-3">
+                    <label class="form-label">New Password</label>
+                    <input class="form-control" type="password" name="passowrd" id="newp" placeholder="New Password">
+                    </div>
+                </div><div class="col-sm-6">
+                    <div class="mb-3">
+                    <label class="form-label">Confirm Password</label>
+                    <input class="form-control" type="text" name="Cpassowrd" id="confirmP" placeholder="New Password">
+                    </div>
+                </div>
+                <div class="mb-3">
+                <label for="formFile" class="form-label">Profile Picture</label>
+                <input class="form-control" type="file" id="formFile">
+                </div>
             </div>
-          </div>
+        </div>
+        <div class="card-footer text-end">
+            <button class="btn btn-primary" type="submit">Update Profile</button>
+        </div>
+    </form>
+    </div>
+    </div>
+</div>
+
 @endsection
 
 @section('script')
+<script>
+function checkForm()
+   {
+    var oldP=document.getElementById("oldP").value;
+    var newP=document.getElementById("newP").value;
+    var confirmP =document.getElementById("confirmP").value;
 
+    if(oldP!=""&&newP!=""&&confirmP!="")
+    {
+      if(oldP!=newP)
+      {
+        if(newP==confirmP)
+         {
+          return true;
+         }
+         else
+          {
+            alert("Confirm password is not same as you new password.");
+            return false;
+          }
+      }
+      else
+     {
+      alert(" This Is Your Old Password,Please Provide A New Password");
+      return false;
+     }
+    }
+    else
+    {
+     alert("All Fields Are Required");
+     return false;
+    }
+}
+</script>
 @endsection
---
+++ resources/views/Admin/account_0.blade.php
@@ -0,0 +1,123 @@
+@extends('Admin.simple.master')
+@section('title', 'Edit Profile')
+
+@section('css')
+@endsection
+
+@section('style')
+@endsection
+
+@section('breadcrumb-title')
+<h3>Edit Profile</h3>
+@endsection
+
+@section('breadcrumb-items')
+<li class="breadcrumb-item">Users</li>
+<li class="breadcrumb-item active">Edit Profile</li>
+@endsection
+
+@section('content')
+<div class="container-fluid">
+    <div class="edit-profile">
+      <div class="row" id="form_preview">
+        <div class="col-xl-4">
+          <div class="card">
+            <div class="card-header">
+              <h4 class="card-title mb-0">My Profile</h4>
+              <div class="card-options"><a class="card-options-collapse" href="#" data-bs-toggle="card-collapse"><i class="fe fe-chevron-up"></i></a><a class="card-options-remove" href="#" data-bs-toggle="card-remove"><i class="fe fe-x"></i></a></div>
+            </div>
+            <div class="card-body">
+                <div class="row mb-2">
+                  <div class="profile-title">
+                    <div class="media"><img class="img-70 rounded-circle" alt="" src="{{ asset('uploads/f2.jpg') }}">
+                      <div class="media-body">
+                        <h5 class="mb-1">{{$data->name}}</h5>
+                        <p>ADMIN</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-8">
+          <div class="card" >
+            <div class="card-header">
+              <h4 class="card-title mb-0">Edit Profile</h4>
+              <div class="card-options"><a class="card-options-collapse" href="#" data-bs-toggle="card-collapse"><i class="fe fe-chevron-up"></i></a><a class="card-options-remove" href="#" data-bs-toggle="card-remove"><i class="fe fe-x"></i></a></div>
+            </div>
+            <div class="card-body">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="mb-3">
+                    <label class="form-label">Name</label>
+                    <input class="form-control" type="text" value="{{$data->name}}" placeholder="Name">
+                  </div>
+                </div>
+
+                <div class="col-sm-6 ">
+                  <div class="mb-3">
+                    <label class="form-label">Email address</label>
+                    <input class="form-control" type="email" value="{{$data->email}}" placeholder="Email">
+                  </div>
+                </div>
+
+               <div class="mb-3">
+                <label for="formFile" class="form-label">Profile Picture</label>
+                <input class="form-control" type="file" id="formFile">
+              </div>
+              </div>
+            </div>
+            <div class="card-footer text-end">
+              <button class="btn btn-primary" type="submit" onclick="update()">Update Profile</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+          <div class="card" >
+            <div class="card-header">
+              <h4 class="card-title mb-0">Edit Profile</h4>
+              <div class="card-options"><a class="card-options-collapse" href="#" data-bs-toggle="card-collapse"><i class="fe fe-chevron-up"></i></a><a class="card-options-remove" href="#" data-bs-toggle="card-remove"><i class="fe fe-x"></i></a></div>
+            </div>
+            <div class="card-body">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="mb-3">
+                    <label class="form-label">Name</label>
+                    <input class="form-control" type="text" value="{{$data->name}}" placeholder="Name">
+                  </div>
+                </div>
+
+                <div class="col-sm-6 ">
+                  <div class="mb-3">
+                    <label class="form-label">Email address</label>
+                    <input class="form-control" type="email" value="{{$data->email}}" placeholder="Email">
+                  </div>
+                </div>
+
+               <div class="mb-3">
+                <label for="formFile" class="form-label">Profile Picture</label>
+                <input class="form-control" type="file" id="formFile">
+              </div>
+              </div>
+            </div>
+            <div class="card-footer text-end">
+              <button class="btn btn-primary" type="submit" onclick="submit()">Update Profile</button>
+            </div>
+          </div>
+      </div>
+    </div>
+</div>
+@endsection
+
+@section('script')
+<script>
+    function update(){
+     var fp = document.getElementById('form_preview');
+     var edit = document.getElementById('edit');
+     fp.style.display('none');
+     edit.style.display('flex');
+    }
+</script>
+@endsection
