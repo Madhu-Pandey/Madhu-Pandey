6df958cb9833f72369567813da8b95e3f5856bd3
Madhu Pandey <madhuuupandey@gmail.com>
2023-04-24 10:11:56 +0530

    v_video(0.7)

---
+++ resources/views/video.blade.php
@@ -0,0 +1,20 @@
+        <!-- Start Video Presentation Area -->
+            <section class="video-presentation-area ptb-100" style="padding-top:30px;">
+                <div class="container">
+                    <div class="section-title">
+                        <h2>Watch this video to know us closely</h2>
+                    </div>
+                    <div class="video-box">
+                        <img src="assets/img/tumbnail1.png" class="main-image" alt="image">
+                        <a href="{{ URL::asset('assets/videos/finalvideo.mp4') }}" class="video-btn popup-youtube"><i
+                                class="bx bx-play"></i></a>
+                        <div class="shape1"><img src="assets/img/shape/1.png" alt="image"></div>
+                        <div class="shape2"><img src="assets/img/shape/2.png" alt="image"></div>
+                        <div class="shape3"><img src="assets/img/shape/3.png" alt="image"></div>
+                        <!-- <div class="shape4"><img src="assets/img/shape/4.png" alt="image"></div>
+                        <div class="shape5"><img src="assets/img/shape/5.png" alt="image"></div> -->
+                        <div class="shape6"><img src="assets/img/shape/6.png" alt="image"></div>
+                    </div>
+                </div>
+            </section>
+        <!-- End Video Presentation Area -->
